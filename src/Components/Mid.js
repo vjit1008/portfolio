a81bded4ed4976fef5d77b46dd7f40cc02c19c8a
vishvajit gadakari <gadakariv@gmail.com>
2023-01-26 12:30:36 +0530

    new

---
+++ src/Components/Mid.js
@@ -0,0 +1,34 @@
+
+// import Avataaar from './Avataaar.svg';
+import Icon from './index1.html'
+
+import'./mid.css'
+
+const Mid =()=>{
+
+return(
+
+<section className="mid-sec">
+  <div className="person-image">
+  {/* <img class="masthead-avatar mb-5" src="./Avataaar.svg.svg" alt="..." /> */}
+  {/* <img src={Avataaar} alt="Avataaar"></img>/ */}
+  </div>
+  <div className="web-page-name">
+      <h1 className='start-name'>VISHVAJIT GADAKARI</h1>
+  </div>
+    
+  <div className="mid-icon">
+    
+ <i class="fas fa-star"></i>
+  </div>
+   <div className="short-overView">
+         <p>
+         Graphic Artist - Web Designer - Illustrator
+         </p>
+  </div>
+
+</section>
+)
+}
+
+export default Mid ;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+
+
+import './navbar.css'
+const Navbar=()=>{
+
+    return(
+         <div className="navbar" > 
+             <h1 className="name">VISHVAJIT GADAKARI</h1>
+          <div className="navbar-manu">
+            <a href="#">PORFOLIO</a>
+            <a href="#">ABOUT</a>
+            <a href="#">CONTACT</a>
+          
+          </div>
+         </div>
+    )
+}
+
+export default Navbar;
